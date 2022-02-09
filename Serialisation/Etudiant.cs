5b02f435bc36a120878cf94eb3867faf6d26e65e
chaimaeBougattaya <chaimaebougattaya1@gmail.com>
2022-02-09 13:02:09 +0100

    Ajoutez des fichiers projet.

---
+++ Serialisation/Etudiant.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Serialisation
+{
+    class Etudiant
+    {
+        static int compteur=0;
+        public int id { get; set; }
+        public string nom{get;set;}
+        public List<Etudiant> amis { get; set; }
+        public Etudiant(string nom)
+        {
+            this.id = ++compteur;
+            this.nom = nom;
+            this.amis = new List<Etudiant>();
+        }
+        public Etudiant() { }
+        public void ajouterAmie(Etudiant e) { this.amis.Add(e); }
+        string detalEtu()
+        {
+            return "\nDétail Etudiant =>   Nom : " + this.nom;
+        }
+        public override string ToString()
+        {
+            string res =detalEtu();
+            res += "\nSes amis : ";
+            foreach(Etudiant e in amis)
+            {
+                res += "\t"+e.detalEtu();
+            }
+            return res;
+        }
+    }
+}
---
+++ Serialisation/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+
+namespace Serialisation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Etudiant e = new Etudiant("Ahmed");
+            e.ajouterAmie(new Etudiant("chaimae"));
+            e.ajouterAmie(new Etudiant("ghita"));
+            e.ajouterAmie(new Etudiant("mourad"));
+
+            //sérialisation => Console 
+            Console.WriteLine("\nSérialisation\n");
+            Console.WriteLine(JsonSerializer.Serialize(e));
+
+            //sérialisation => dans un fihier etudiants.json 
+            Console.WriteLine("\nSérialisation dans le fichier\n");
+            File.WriteAllText("etudiants.json", JsonSerializer.Serialize(e));
+
+
+            //désérialisation
+            Console.WriteLine("\ndéserialisation\n");
+
+            Etudiant es = JsonSerializer.Deserialize<Etudiant>(File.ReadAllText("etudiants.json"));
+            Console.WriteLine(es.ToString());
+
+            Console.WriteLine("\nfin\n");
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
